efe555baa6ff984258c715d5cbf7810479222501
HARSH HARSH <harshji511@gmail.com>
2025-07-10 00:15:34 +0530

    final updated project

--- client/src/components/Rightsidebar.jsx
+++ client/src/components/Rightsidebar.jsx
@@ -1,12 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import assets from '../assets/assets';
 import { imagesDummyData } from '../assets/assets.js';
 import { AuthContext } from '../../context/AuthContext.jsx';
+import { ChatContext } from '../../context/ChatContext.jsx';
  
 const RightSidebar = ({ selectedUser }) => {
-  const { logout } = useContext(AuthContext);
+  const { logout ,onlineUsers} = useContext(AuthContext);
+  const { messages} = useContext(ChatContext);
+  const [imagemessages,setimagemessages]=useState([]);
+  
+  useEffect(() => {
+    // Filter messages that contain an image (depending on your structure)
+    const onlyImages = messages.filter(a =>a.image).map(a =>a.image);
+    setimagemessages(onlyImages);
+  }, [messages]);
 
   if (!selectedUser) return null; // Render nothing if no user is selected
+const isOnline =
+    Array.isArray(onlineUsers) &&
+    selectedUser &&
+    selectedUser._id &&
+    onlineUsers.includes(selectedUser._id.toString());
 
   return (
     <div className="h-full w-full  text-white relative flex flex-col border-l border-gray-400">
@@ -21,7 +35,12 @@ const RightSidebar = ({ selectedUser }) => {
           />
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
             {selectedUser?.fullName}
-            <span className="w-2 h-2 rounded-full bg-green-500"></span>
+             <span
+            className={`h-3 w-3 rounded-full ml-2 border-2 border-[#282142] ${
+              isOnline ? 'bg-green-500' : 'bg-gray-500'
+            }`}
+            title={isOnline ? 'Online' : 'Offline'}
+          ></span>
           </h1>
           <p className="px-10 mx-auto">{selectedUser?.bio}</p>
         </div>
@@ -32,8 +51,8 @@ const RightSidebar = ({ selectedUser }) => {
         {/* Media Section */}
         <div className="px-5 text-xs">
           <p className="mb-2">Media</p>
-          <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
-            {imagesDummyData.map((url, index) => (
+          <div className="mt-2 max-h-[400px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80 scrollbar-hide">
+            {imagemessages.map((url, index) => (
               <div
                 key={index}
                 onClick={() => window.open(url)}
@@ -51,7 +70,8 @@ const RightSidebar = ({ selectedUser }) => {
       </div>
 
       {/* Logout Button */}
-      <div className="border-t border-gray-700 p-4">
+      <div   className="absolute bottom-0 left-0 w-full flex items-center gap-3 p-4 bg-[#282142] border-t border-gray-600 z-10"
+        style={{ height: "50px" }}>
         <button
           onClick={(e) => {
             e.stopPropagation();
